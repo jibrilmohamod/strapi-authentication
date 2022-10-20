de4d71ab0881010d213b34aef30fde2dd889b24e
jibril <jazimjibril@gmail.com>
2022-10-20 19:57:09 +0300

    made changes ⚡

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from "./App.vue"
 import router from "./router"
 
 import "./assets/main.css"
-
+import "vue-toastification/dist/index.css"
 const app = createApp(App)
 
 app.use(createPinia())
