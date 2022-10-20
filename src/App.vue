06a1b07cfd52c7a38e176d9087870a070b7fedc8
jibril <jazimjibril@gmail.com>
2022-10-20 11:17:09 +0300

    made changes ⚡

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
- import { RouterLink, RouterView } from "vue-router"
+ import { RouterView } from "vue-router"
 </script>
 
 <template>
  <header>
-  <img
-   alt="Vue logo"
-   class="logo"
-   src="@/assets/logo.svg"
-   width="125"
-   height="125"
-  />
-
   <div class="wrapper">
-   <nav>
-    <RouterLink class="text-red-600" to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-   </nav>
+   <TheNav></TheNav>
   </div>
  </header>
 
